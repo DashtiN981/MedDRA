efedfb1aecc003d0b00c56cba6a23ad5203e271f
Naghme <naghme.dashti@gmail.com>
2026-02-11 09:48:33 +0100

    transfer private data

--- data/Excel files/convert_excell_CSV.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Load Excel file with delimiter handling
-df = pd.read_excel('/home/naghmedashti/MedDRA-LLM/data/Excel files/soc_translation_de_en.xlsx')
-
-# Save as CSV
-df.to_csv('/home/naghmedashti/MedDRA-LLM/data/soc_translation_de_en.csv', sep=';', index=False)
